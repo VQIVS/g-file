d92302fa969faef572b56a0046ce9afe36737f32
AmirMahdi Qiasvand <119480978+VQIVS@users.noreply.github.com>
2024-07-07 17:17:36 +0330

    Revert "Feature/accounts/models"

--- core/services/accounts/api/v1/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import viewsets
-from services.accounts.models import User
-from .serializers import UserSerializer
-
-
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
